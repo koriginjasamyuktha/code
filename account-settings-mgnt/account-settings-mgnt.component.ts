b1a77a9163665fad29729d9b5fea60d296cbd961
koriginjasamyuktha <samyukthak01@gmail.com>
2021-06-07 05:31:06 -0700

    Add files via upload

---
+++ account-settings-mgnt/account-settings-mgnt.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-account-settings-mgnt',
+  templateUrl: './account-settings-mgnt.component.html',
+  styleUrls: [],
+  providers: []
+})
+export class AccountSettingsMgntComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
